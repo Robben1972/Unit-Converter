ab9985b2b1d74c1ad61c92a69b49a36e92ba9685
Robben1972 <bozorovshahob27@gmail.com>
2025-03-27 00:19:24 +0500

    Added temperature

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from lenght import router as lenght_router
 from weight import router as weight_router
+from temperature import router as temperature_router
 
 app = FastAPI()
 
 app.include_router(lenght_router)
 app.include_router(weight_router)
+app.include_router(temperature_router)
